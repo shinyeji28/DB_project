82ef5780ba837dafb734c28e9dd9af456101572b
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2020-12-12 19:42:12 +0900

    page.py +로그인 + 회원가입
    로그인고 회원가입 페이지 추가 + 데이터 베이스에 입력 + u에서 에러

--- page.py
+++ page.py
@@ -1,14 +1,19 @@
 from flask import Flask, flash, render_template, request, redirect, url_for
-import sqlite3, sys
+import sqlite3 
+import sys
 
 app = Flask(__name__)
 app.secret_key = 'abcd'
 
-@app.route('/login')
-def login():
+@app.route('/')
+def first_page():    
+    return render_template('first.html')
+
+@app.route('/login',methods=['GET','POST'])
+def user_login():
     return render_template('login.html')
 
-@app.route('/exhib')
+@app.route('/exhib', methods=['GET','POST'])
 def showexhib():
     db = sqlite3.connect("DB_project_data.db")
     db.row_factory = sqlite3.Row
@@ -23,7 +28,7 @@ def editMenu():
     if request.method=='POST':
         db = sqlite3.connect("DB_project_data.db")
         db.row_factory = sqlite3.Row
-        query = "SELECT ID,pw FROM USERS WHERE ID = ? AND pw = ?"
+        query = "SELECT ID,pw FROM USERS WHERE ID = ? and pw=?"
         value=(request.form['uid'],request.form['pw'])
         data=db.execute(query,value).fetchall()
         db.close()
@@ -31,7 +36,39 @@ def editMenu():
             return redirect(url_for('showexhib'))
         else:
             flash('회원 정보가 없습니다.')
-            return redirect(url_for('login'))
+            return redirect(url_for('user_login'))
+
+@app.route('/assign', methods=['GET','POST'])
+def user_assign():
+    return render_template('signup.html')
+
+@app.route('/save', methods=['GET','POST'])
+def save_users():
+    if  request.method=='POST':
+        db = sqlite3.connect("DB_project_data.db")
+        db.row_factory = sqlite3.Row
+        query = "SELECT ID,pw FROM USERS WHERE ID = ? and pw=?"
+        value=(request.form['uid'],request.form['pw'])
+        data=db.execute(query,value).fetchall()
+        db.close()
+        if data:
+            flash('이미 존재하는 ID입니다.')
+            return redirect(url_for('user_login'))
+        else:
+            db = sqlite3.connect("DB_project_data.db")
+            db.row_factory = sqlite3.Row
+            u=db.execute("SELECT count(*)+1 as count from USERS").fetchall()
+            db.close()
+
+            db = sqlite3.connect("DB_project_data.db") 
+            db.row_factory = sqlite3.Row
+            db.execute("INSERT INTO USERS(uID, ID,pw,uName) values( ?,?,?,?)",(u,request.form['uid'],request.form['pw'],request.form['Name']))
+            db.commit()
+            db.close()
+            return redirect(url_for('user_login'))
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
