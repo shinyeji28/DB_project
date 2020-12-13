d849918e9054d06cf7c896574861ae80117701c8
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2020-12-13 15:57:02 +0900

    + detail_info + comment
    특정 전시 정보와 사용자가 입력한 comment를 보여주고 DB에 저장

--- page.py
+++ page.py
@@ -5,6 +5,8 @@ import sys
 app = Flask(__name__)
 app.secret_key = 'abcd'
 
+USER_NAME="qw"
+
 @app.route('/')
 def first_page():    
     return render_template('first.html')
@@ -28,10 +30,11 @@ def editMenu():
     if request.method=='POST':
         db = sqlite3.connect("DB_project_data.db")
         db.row_factory = sqlite3.Row
-        query = "SELECT ID,pw FROM USERS WHERE ID = ? and pw=?"
+        query = "SELECT ID,pw,uName FROM USERS WHERE ID = ? and pw=?"
         value=(request.form['uid'],request.form['pw'])
         data=db.execute(query,value).fetchall()
         db.close()
+
         if data:
             return redirect(url_for('showexhib'))
         else:
@@ -55,20 +58,37 @@ def save_users():
             flash('이미 존재하는 ID입니다.')
             return redirect(url_for('user_login'))
         else:
-            db = sqlite3.connect("DB_project_data.db")
-            db.row_factory = sqlite3.Row
-            u=db.execute("SELECT count(*)+1 as count from USERS").fetchall()
-            db.close()
-
             db = sqlite3.connect("DB_project_data.db") 
             db.row_factory = sqlite3.Row
-            db.execute("INSERT INTO USERS(uID, ID,pw,uName) values( ?,?,?,?)",(u,request.form['uid'],request.form['pw'],request.form['Name']))
+            db.execute("INSERT INTO USERS(ID,pw,uName) values(?,?,?)",(request.form['uid'],request.form['pw'],request.form['Name']))
             db.commit()
             db.close()
-            return redirect(url_for('user_login'))
+            return redirect(url_for('showexhib'))
+
+@app.route('/detail_information/<int:e>', methods=['GET', 'POST'])
+def detail_info(e):
+    db = sqlite3.connect("DB_project_data.db")
+    db.row_factory = sqlite3.Row
+    item = db.execute(
+        'select * from EXHIBITION where eID=?',(e,)
+    ).fetchall()
+    db.close()
+    return render_template('detail.html',items=item)
 
 
+@app.route('/comment/<int:eID>', methods=['GET', 'POST'])
+def comment(eID):
+    db = sqlite3.connect("DB_project_data.db")
+    c=db.cursor()
+    
+    data=c.execute('select count(*)+1 from COMMENT').fetchone()
 
+    for r in data:
+        db.execute("INSERT INTO COMMENT(cID,cName,content,eID) values(?,?,?,?)",(r, USER_NAME,request.form['user_message'],eID))
+    db.commit()
+    db.close()
+    return redirect(url_for('detail_info',e=eID))
+    
 
 if __name__ == '__main__':
     app.debug = True
