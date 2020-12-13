76918cbf4dacfba4974357c7abbe85651ae214f7
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2020-12-13 22:53:28 +0900

    Update page.py

--- page.py
+++ page.py
@@ -1,80 +1,145 @@
-<!DOCTYPE html>
-<html>
-    <body>
-  
-        {% with messages = get_flashed_messages() %}
-        {% if messages %}
-            <ul class=flashes>
-            {% for message in messages %}
-            <li>{{ message }}</li>
-            {% endfor %}
-            </ul>
-        {% endif %}
-        {% endwith %}
+from flask import Flask, flash, render_template, request, redirect, url_for
+import sqlite3 
+import sys
 
-        <form method = 'POST'>
-        <p>인원수를 선택하세요</p>
-        <a>어린이&nbsp;&nbsp;</a><select name = "child" id = "child_p">
-            <option value = "0">0명</option>
-            <option value = "1">1명</option>
-            <option value = "2">2명</option>
-            <option value = "3">3명</option>
-            <option value = "4">4명</option>
-            <option value = "5">5명</option>
-            <option value = "6">6명</option>
-            <option value = "7">7명</option>
-            <option value = "8">8명</option>
-            <option value = "9">9명</option>
-            <option value = "10">10명</option>
-        </select><br><br>
-        <a>청소년&nbsp;&nbsp;</a><select name = "teen" id = "teen_p">
-            <option value = "0">0명</option>
-            <option value = "1">1명</option>
-            <option value = "2">2명</option>
-            <option value = "3">3명</option>
-            <option value = "4">4명</option>
-            <option value = "5">5명</option>
-            <option value = "6">6명</option>
-            <option value = "7">7명</option>
-            <option value = "8">8명</option>
-            <option value = "9">9명</option>
-            <option value = "10">10명</option>
-        </select><br><br>
-        <a>성인&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a><select name = "adult" id = "adult_p">
-            <option value = "0">0명</option>
-            <option value = "1">1명</option>
-            <option value = "2">2명</option>
-            <option value = "3">3명</option>
-            <option value = "4">4명</option>
-            <option value = "5">5명</option>
-            <option value = "6">6명</option>
-            <option value = "7">7명</option>
-            <option value = "8">8명</option>
-            <option value = "9">9명</option>
-            <option value = "10">10명</option>
-        </select><br><br>
-        <p>티켓 수령 방법</p>
-        <input type = "radio" name = "ticket" value = "0" checked>현장 수령
-        <input type = "radio" name = "ticket" value = "2800">배송(2,800원)
-        
-        <p>예매자 확인</p>
-        {% for i in userInfo %}
-        <a>이름 : </a>{{i.uName}}<br>
-        생년월일 : <input type = text name = "birth" value = "예) 991111">
-        <a>(생년월일이 일치하지 않을 시 예매가 불가합니다.)</a><br>
-        <a>연락처 : </a>{{i.phoneNumber}}<br>
-        <a>이메일 : </a>{{i.eMail}}<br>
-        {% endfor %}
+app = Flask(__name__)
+app.secret_key = 'abcd'
 
-        <input type = 'submit'  value = '조회하기' onkeyup='aa'>        
+USER_NAME="qw"
+
+@app.route('/')
+def first_page():    
+    return render_template('first.html')
+
+@app.route('/login',methods=['GET','POST'])
+def user_login():
+    return render_template('login.html')
+
+@app.route('/<string:ID>/exhib', methods=['GET','POST'])
+def showexhib(ID):
+    db = sqlite3.connect("DB_project_data.db")
+    db.row_factory = sqlite3.Row
+    items = db.execute(
+        'select * from EXHIBITION'
+    ).fetchall()
+    db.close()
+    return render_template('context.html',items=items,ID2 = ID)
+
+@app.route('/exhib/edit', methods=['GET','POST'])
+def editMenu():
+    if request.method=='POST':
+        db = sqlite3.connect("DB_project_data.db")
+        db.row_factory = sqlite3.Row
+        query = "SELECT ID,pw,uName FROM USERS WHERE ID = ? and pw=?"
+        value=(request.form['uid'],request.form['pw'])
+        data=db.execute(query,value).fetchall()
+        db.close()
+
+        if data:
+            return redirect(url_for('showexhib',ID=request.form['uid']))
+        else:
+            flash('회원 정보가 없습니다.')
+            return redirect(url_for('user_login'))
+
+@app.route('/assign', methods=['GET','POST'])
+def user_assign():
+    return render_template('signup.html')
+
+@app.route('/save', methods=['GET','POST'])
+def save_users():
+    if  request.method=='POST':
+        db = sqlite3.connect("DB_project_data.db")
+        db.row_factory = sqlite3.Row
+        query = "SELECT ID,pw FROM USERS WHERE ID = ? and pw=?"
+        value=(request.form['uid'],request.form['pw'])
+        data=db.execute(query,value).fetchall()
+        db.close()
+        if data:
+            flash('이미 존재하는 ID입니다.')
+            return redirect(url_for('user_login'))
+        else:
+            db = sqlite3.connect("DB_project_data.db") 
+            db.row_factory = sqlite3.Row
+            db.execute('INSERT INTO USERS(ID,pw,uName,birthday, phoneNumber, eMail) values(?,?,?,?,?,?)'
+                    ,(request.form['uid'],request.form['pw'],request.form['Name'],request.form['birth'],request.form['phone'],request.form['email']))
+            db.commit()
+            db.close()
+            return redirect(url_for('showexhib',ID=request.form['uid']))
+
+@app.route('/detail_information/<int:e>/<string:ID3>', methods=['GET', 'POST'])
+def detail_info(e,ID3):
+    db = sqlite3.connect("DB_project_data.db")
+    db.row_factory = sqlite3.Row
+    item = db.execute(
+        'select * from EXHIBITION where eID=?',(e,)
+    ).fetchall()
+    data=db.execute('select cName,content from COMMENT where eID=?',(e,)).fetchall()
+    db.close()
+    return render_template('detail.html',items=item,ID4=ID3,datas=data)
 
 
-        <p>총 결제 금액 : </p>{{results}}
-        <input type = 'submit'  value = '예매하기' formaction="{{url_for('reserve_accepct',ID8=ID7,e=eID)}}">
-        
-        
+@app.route('/comment/<int:eID>/<string:ID5>', methods=['GET', 'POST'])
+def comment(eID,ID5):
+    db = sqlite3.connect("DB_project_data.db")
+    c=db.cursor()
+    data=c.execute('select count(*)+1 from COMMENT').fetchone()
 
-        
-        </form>
-    </body>
-</html>
+    for r in data:
+        db.execute("INSERT INTO COMMENT(cID,cName,content,eID) values(?,?,?,?)",(r, ID5,request.form['user_message'],eID))
+    db.commit() 
+    db.close()
+    return redirect(url_for('detail_info',e=eID,ID3=ID5))
+
+
+@app.route('/reserve/<string:ID6>/<int:e>' , methods=['GET','POST'])
+def reserve(ID6,e):  
+    db = sqlite3.connect("DB_project_data.db")
+    db.row_factory = sqlite3.Row
+    c=db.cursor()
+    datas=c.execute('select * from USERS where ID=?',(ID6,)).fetchall()
+    db.close()
+    return render_template('reserve.html',ID7=ID6,userInfo=datas,eID=e)
+
+@app.route('/reserve_accepct/<string:ID8>/<int:e>' , methods=['GET','POST'])
+def reserve_accepct(ID8,e):  
+    db = sqlite3.connect("DB_project_data.db")
+    db.row_factory = sqlite3.Row
+    c=db.cursor()
+    data=c.execute('select count(*)+1 from RESERVATION').fetchone()
+    for r in data:
+        db.execute('INSERT INTO RESERVATION(rID,uID,eID,childNum,teenNum,adultNum,delivery,totalPrice) values(?,?,?,?,?,?,?,?)'
+        ,(r, ID8,e,request.form['child'],request.form['teen'],request.form['adult'],request.form['ticket'],500))
+    db.commit()
+    db.close()
+
+    return redirect(url_for('showexhib',ID=ID8))
+
+#@app.route('/inquiry/<string:ID9>',method=['GET','POST'])
+#def inquiry(ID9):
+#    db = sqlite3.connect("DB_project_data.db")
+#    db.row_factory = sqlite3.Row
+#    c=db.cursor()
+#    datas=c.execute('select * from RESERVATION where uID=?',(ID9,)).fetchall()
+    
+@app.route('/aa/<int:e>', methods=['GET', 'POST'])
+def aa(e):
+    db = sqlite3.connect("DB_project_data.db")
+    #db.row_factory = sqlite3.Row
+    a=db.cursor()
+    data = a.execute('select childPrice,teenPrice, adultPrice from EXHIBITION where eID=?',(e,)).fetchone()
+    db.close()
+    child,teen,adult=data
+    childnum = request.form['child']
+    teennum = request.form['teen']
+    adultnum = request.form['adult']
+    delivery = request.form['ticket']
+    result = int(float(child))*int(childnum)+int(float(teen))*int(teennum)+int(float(adult))*int(adultnum)+int(float(delivery))
+    print(result)
+    flash(result)
+    return redirect('reserve',results=result,eID=e)
+    #return ('',204)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
